c89ce8c42f4753159681ccb3c402c05bf82f9780
npp-20 <71215755+npp-20@users.noreply.github.com>
2021-11-08 17:06:03 +0800

    Add files via upload

---
+++ ideaGitDemo/src/main/java/com/acca/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.acca.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+//		System.out.println("git for master code change");
+//		System.out.println("git for hot-fix code change");
+		System.out.println("git for hot-fix code change2");
+		System.out.println("git for website code change");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
